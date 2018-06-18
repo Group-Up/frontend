6d45b7d5435e6a92f3acf85b7b891a7ed28917a0
Sarah Geyer <sjgeyer@comcast.net>
2018-06-18 13:11:43 -0700

    remove extra route

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -2,4 +2,4 @@ export const LANDING = '/';
 export const LOGIN = '/login';
 export const SIGNUP = '/signup';
 export const DASHBOARD = '/dashboard';
-export const GOOGLE_LOGIN = '/oauth/google';
+// export const GOOGLE_LOGIN = '/oauth/google';
