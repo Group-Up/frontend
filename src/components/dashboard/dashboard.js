bbf10d71bbae1f69cbb3245af0a09f48094c0250
Sarah Geyer <sjgeyer@comcast.net>
2018-06-19 15:25:41 -0700

    add post form

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import { WELCOME } from '../../text';
 import Profile from '../profile/profile';
 import * as profileActions from '../../actions/profile';
 import * as postActions from '../../actions/posts';
+import PostItem from '../post-item/post-item';
 
 class Dashboard extends React.Component {
   componentDidMount() {
@@ -17,13 +17,23 @@ class Dashboard extends React.Component {
     }
   }
   render() {
-    const { profile, posts } = this.props;
+    const { profile, posts, events } = this.props;
     const username = profile && profile.username;
     return (
       <div className='dashboard'>
         <h1>Dashboard</h1>
         <h2>Welcome { username && username }</h2>
         <Profile profile={profile}/>
+        <h3>My Events:</h3>
+        {
+          /* TODO: ADD EVENT ITEMS HERE */
+          <p>No events to display</p>
+        }
+        <h3>Recent Posts:</h3>
+        {
+          posts.length > 0 ? posts.map(post => <PostItem post={post} key={post._id}/>) :
+            <p>No posts to display</p>
+        }
       </div>
     );
   }
@@ -36,12 +46,14 @@ Dashboard.propTypes = {
   pFetchUserProfile: PropTypes.func,
   pFetchPosts: PropTypes.func,
   posts: PropTypes.array,
+  events: PropTypes.array,
 };
 
 const mapStateToProps = state => ({
   profile: state.profile,
   loggedIn: !!state.token,
   posts: state.posts,
+  events: state.events,
 });
 
 const mapDispatchToProps = dispatch => ({
