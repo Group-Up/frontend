70c8f3eca2c071a630088c0ecd25b9b35c4a3a8a
Sarah Geyer <sjgeyer@comcast.net>
2018-06-21 14:12:09 -0700

    update google redirect constant to include global API_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
+
 export const TOKEN_COOKIE_KEY = 'GU-Token';
-export const GOOGLE_LOGIN_REDIRECT = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://localhost:3000/oauth/google&scope=profile%20email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcontacts&client_id=748380772658-s92lpugvb245s1n5inds0dgr845eocc6.apps.googleusercontent.com&prompt=consent&response_type=code'; // TODO: needs to change
 export const CREATE_ACCOUNT = 'Don\'t have an account? Create one.';
 export const EMAIL_SUBJECT = 'Hello from GroupUp!';
 export const EMAIL_BODY = 'Please join the app {group up site link here}'; // TODO: change
-
+export const GOOGLE_LOGIN_REDIRECT = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${API_URL}/oauth/google&scope=profile%20email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcontacts&client_id=748380772658-s92lpugvb245s1n5inds0dgr845eocc6.apps.googleusercontent.com&prompt=consent&response_type=code`;
